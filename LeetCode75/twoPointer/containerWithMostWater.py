3ee3c5e2ab6cf40cb6cbf2769b1fbfcd65dbaf75
swekshajha12 <jhasweksha12@gmail.com>
2023-10-01 14:34:23 +0530

    [ADD] code for container with most water

---
+++ LeetCode75/twoPointer/containerWithMostWater.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75
+
+# we'll use 2 pointer approach here also
+
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        p1, p2 = 0, len(height) - 1
+        max_area = 0
+        while p1 < p2:
+            area = (p2 - p1) * min(height[p1], height[p2])
+            if area > max_area:
+                max_area = area
+
+            if height[p1] < height[p2]:
+                p1 += 1
+            else:
+                p2 -= 1
+
+        return max_area
+
+
+ob = Solution()
+print(ob.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
