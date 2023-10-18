e0dfd33a579f4ac129297a92abf12e3438f9b142
swekshajha12 <jhasweksha12@gmail.com>
2023-10-18 13:30:19 +0530

    [ADD] brute force solution for lpsubstr

--- longestPalindromicSubstring.py
+++ longestPalindromicSubstring.py
@@ -5,32 +5,61 @@ class Solution:
         res = ""
         res_len = 0
         for i in range(len(s)):
-            #odd length
-            l, r = i,i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if(r-l+1)>res_len:
-                    res_len = r-l+1
-                    res = s[l:r+1]
-                l-=1
-                r+=1
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > res_len:
+                    res_len = r - l + 1
+                    res = s[l:r + 1]
+                l -= 1
+                r += 1
 
-            #even length
-            l,r=i,i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if(r-l+1)>res_len:
-                    res_len = r-l+1
-                    res = s[l:r+1]
-                l-=1
-                r+=1
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > res_len:
+                    res_len = r - l + 1
+                    res = s[l:r + 1]
+                l -= 1
+                r += 1
         return res
+
+    def checkPalindrome(self, s):
+        n = len(s)
+        l, r = 0, n - 1
+
+        while (l < r):
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+                continue
+            else:
+                return False
+        return True
+
+    def longestPalindromicSubstringBruteForce(self, s: str):
+        n = len(s)
+        res = []
+        substrs = []
+        for i in range(n):
+            substrs.append(s[0:i + 1])
+
+        for substr in substrs:
+            isPalin = self.checkPalindrome(substr)
+            if isPalin:
+                res.append(len(substr))
+            else:
+                res.append(1)
+        print(res)
+        return max(res)
+
 
 # expand approach
 ob = Solution()
 print(ob.longestPalindrome("babad"))
 print(ob.longestPalindrome("ac"))
 print(ob.longestPalindrome("abb"))
-
-
+print(ob.longestPalindromicSubstringBruteForce("babad"))
 
 # class Solution:
 #     def longestPalindrome(self, s: str) -> str:
