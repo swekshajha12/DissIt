6c0decb64f3256daa6024a0d7e40d1e17c518178
swekshajha12 <jhasweksha12@gmail.com>
2023-11-08 14:43:53 +0530

    ADD solution for strict peak element

--- LeetCode75/binarySearch/findPeakElement.py
+++ LeetCode75/binarySearch/findPeakElement.py
@@ -23,9 +23,53 @@ class Solution:
 
         return mid
 
+    #  This solution will also work for when you when to find an element
+    # where all elements to its left or right is smaller than that element
+    def findPeakElementApp2(self, arr: List[int]) -> int:
+        n = len(arr)
+        mid = n // 2
+        l = mid - 1
+        r = mid + 1
+
+        while l >= 0 and r < n:
+            if arr[l] <= arr[mid] and arr[r] <= arr[mid]:
+                l -= 1
+                r += 1
+            elif arr[l] > arr[mid] and arr[r] > arr[mid]:
+                if arr[l] > arr[r]:
+                    mid = l
+                    l -= 1
+                else:
+                    mid = r
+                    r += 1
+
+            elif arr[l] > arr[mid]:
+                mid = l
+                l -= 1
+            elif arr[r] > arr[mid]:
+                mid = r
+                r += 1
+        if r >= n and l >= 0:
+            if arr[l] > arr[mid]:
+                mid = l
+
+        elif r < n and l < 0:
+            if arr[r] > arr[mid]:
+                mid = r
+
+        return mid
+
 
 ob = Solution()
+print(ob.findPeakElement([4, 2, 3, 1, 5]))
 print(ob.findPeakElement([1, 2, 3, 1]))
 print(ob.findPeakElement([1, 2, 1, 3, 5, 6, 4]))
 print(ob.findPeakElement([1]))
 print(ob.findPeakElement([1, 2, 3, 4, 5, 6]))
+print(ob.findPeakElementApp2([1, 2, 3, 1]))
+print(ob.findPeakElementApp2([1, 2, 1, 3, 5, 6, 4]))
+print(ob.findPeakElementApp2([1]))
+print(ob.findPeakElementApp2([1, 2, 3, 4, 5, 6]))
+print(ob.findPeakElementApp2([2, 1]))
+print(ob.findPeakElementApp2([1, 2]))
+print(ob.findPeakElementApp2([6, 5, 4, 3, 2, 3, 2]))
