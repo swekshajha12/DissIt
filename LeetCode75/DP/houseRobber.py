5e0196192a1be91f3e888cf47e6a06cdecce0f6b
swekshajha12 <jhasweksha12@gmail.com>
2023-11-11 23:46:12 +0530

    [ADD] code for houserobber

--- LeetCode75/DP/houseRobber.py
+++ LeetCode75/DP/houseRobber.py
@@ -4,6 +4,41 @@ from typing import List
 
 
 class Solution:
+
+    def rob_recursive(self, nums):
+
+        def solve(nums, n):
+            if n <= 0:
+                return 0
+            return max(nums[n - 1] + solve(nums, n - 2), solve(nums, n - 1))
+
+        return solve(nums, len(nums))
+
+    def rob_recursive_memoisation(self, nums):
+        dp = [0 for _ in range(len(nums))]
+
+        def solve(nums, n):
+            if n <= 0:
+                return 0
+            if dp[n - 1] != 0:
+                return dp[n - 1]
+            dp[n - 1] = max(nums[n - 1] + solve(nums, n - 2), solve(nums, n - 1))
+            return dp[n - 1]
+
+        return solve(nums, len(nums))
+
+    def rob_tab(self, nums):
+        dp = [0 for _ in range(len(nums))]
+
+        def solve(nums, n):
+            if n <= 0:
+                return 0
+            for i in range(n - 1, -1, -1):
+                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            return dp[0]
+
+        return solve(nums, len(nums))
+
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [0] * len(nums)
@@ -19,3 +54,6 @@ class Solution:
 
 ob = Solution()
 print(ob.rob([1, 2, 3, 1]))
+print(ob.rob_recursive([1, 2, 3, 1]))
+print(ob.rob_recursive_memoisation([1, 2, 3, 1]))
+print(ob.rob_tab([1, 2, 3, 1]))
