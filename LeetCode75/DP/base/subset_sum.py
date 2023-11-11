3b6fe32c490d9cf94416b4b548090778d3adc45c
swekshajha12 <jhasweksha12@gmail.com>
2023-11-11 12:08:37 +0530

    [ADD] subset sum solution

---
+++ LeetCode75/DP/base/subset_sum.py
@@ -0,0 +1,39 @@
+class Solution:
+    def __init__(self, n, k):
+        self.dp = [[-1 for _ in range(k + 1)] for _ in range(n + 1)]
+
+    def subset_sum_recursive(self, arr, target, n):
+        if n == 0 and target != 0:
+            return False
+        if target == 0:
+            return True
+
+        if arr[n - 1] <= target:
+            return self.subset_sum_recursive(arr, target - arr[n - 1], n - 1) or self.subset_sum_recursive(arr, target,
+                                                                                                           n - 1)
+        else:
+            return self.subset_sum_recursive(arr, target, n - 1)
+
+    def subset_sum_recursive_with_memoization(self, arr, target, n):
+        if n == 0 and target != 0:
+            return False
+        if target == 0:
+            return True
+
+        if self.dp[n][target] != -1:
+            return self.dp[n][target]
+
+        if arr[n - 1] <= target:
+            self.dp[n][target] = self.subset_sum_recursive_with_memoization(arr, target - arr[n - 1],
+                                                                            n - 1) or self.subset_sum_recursive_with_memoization(
+                arr, target,
+                n - 1)
+        else:
+            self.dp[n][target] = self.subset_sum_recursive_with_memoization(arr, target, n - 1)
+
+        return self.dp[n][target]
+
+
+ob = Solution(5, 11)
+# print(ob.subset_sum_recursive([2, 3, 7, 8, 10], 11, 5))
+print(ob.subset_sum_recursive_with_memoization([2, 3, 7, 8, 10], 11, 5))
