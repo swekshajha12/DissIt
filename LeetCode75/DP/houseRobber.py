8a55dd54937f8d6eec57301edc9c56ffc0388722
swekshajha12 <jhasweksha12@gmail.com>
2023-11-13 16:00:46 +0530

    [ADD] solutions

--- LeetCode75/DP/houseRobber.py
+++ LeetCode75/DP/houseRobber.py
@@ -27,6 +27,9 @@ class Solution:
 
         return solve(nums, len(nums))
 
+
+    # this is wrong, for tabulisation db array, try to do it
+    # from start to end
     def rob_tab(self, nums):
         dp = [0 for _ in range(len(nums))]
 
@@ -35,6 +38,7 @@ class Solution:
                 return 0
             for i in range(n - 1, -1, -1):
                 dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            print(dp)
             return dp[0]
 
         return solve(nums, len(nums))
@@ -51,9 +55,49 @@ class Solution:
 
         return dp[n - 1]
 
+    def rob_recursive_k(self, nums, k):
+        def solve(nums, n):
+            if n <= 0:
+                return 0
+            return max(nums[n - 1] + solve(nums, n - 1 - k), solve(nums, n - 1))
+
+        return solve(nums, len(nums))
+
+    def rob_recursive_with_memoization_k(self, nums, k):
+        dp = [float('-inf')] * len(nums)
+
+        def solve(nums, n):
+            if n <= 0:
+                return 0
+            if dp[n - 1] != float('-inf'):
+                return dp[n - 1]
+            dp[n - 1] = max(nums[n - 1] + solve(nums, n - 1 - k), solve(nums, n - 1))
+            return dp[n - 1]
+
+        return solve(nums, len(nums))
+
+    def rob_recursive_tab_k(self, nums, k):
+        n = len(nums)
+        dp = [0] * len(nums)
+        max_till_now = float('-inf')
+        if n <= 0:
+            return 0
+        dp[0]=nums[0]
+        for i in range(1,k):
+            max_till_now = max(nums[i], max_till_now)
+            dp[i]=max_till_now
+
+        for i in range(k, len(nums)):
+            dp[i] = max(nums[i] + dp[i - k-1], dp[i-1])
+        print(dp)
+        return dp[n-1]
+
 
 ob = Solution()
-print(ob.rob([1, 2, 3, 1]))
-print(ob.rob_recursive([1, 2, 3, 1]))
-print(ob.rob_recursive_memoisation([1, 2, 3, 1]))
-print(ob.rob_tab([1, 2, 3, 1]))
+# print(ob.rob([1, 2, 3, 1]))
+# print(ob.rob_recursive([1, 2, 3, 1]))
+# print(ob.rob_recursive_k([1, 2, 3, 1], 2))
+print(ob.rob_recursive_with_memoization_k([1, 2, 3, 1, 5], 1))
+print(ob.rob_recursive_tab_k([1, 2, -3, -1, 5], 1))
+# print(ob.rob_recursive_memoisation([1, 2, 3, 1]))
+print(ob.rob_tab([1, 2, 3, 1,5]))
--- LeetCode75/DP/minCostClimbingStairs.py
+++ LeetCode75/DP/minCostClimbingStairs.py
@@ -5,6 +5,43 @@ from typing import List
 
 class Solution:
 
+    def recursive(self, cost):
+
+        def solve(cost, i, n):
+            if n == 0 or i >= n:
+                return 0
+            return cost[i] + min(solve(cost, i + 1, n), solve(cost, i + 2, n))
+
+        return min(solve(cost, 0, len(cost)), solve(cost, 1, len(cost) - 1))
+
+    def recursive_with_memoization(self, cost):
+        dp = [0] * (len(cost) + 1)
+        dp[0], dp[1] = cost[0], cost[1]
+
+        def solve(cost, i, n):
+            if n == 0:
+                return cost[i]
+            if n < 0 or i >= n:
+                return 0
+            if dp[i] != 0:
+                return dp[i]
+
+            dp[i] = cost[i] + min(solve(cost, i + 1, n), solve(cost, i + 2, n))
+            return dp[i]
+
+        return min(solve(cost, 0, len(cost)), solve(cost, 1, len(cost) - 1))
+
+    def tab(self, cost):
+        dp = [0] * len(cost)
+        n = len(cost)
+        if n <= 0:
+            return 0
+        dp[0], dp[1] = cost[0], min(cost[0] + cost[1], cost[1])
+        for i in range(2, n):
+            dp[i] = min(cost[i] + dp[i - 1], cost[i] + dp[i - 2])
+
+        return dp[n - 1]
+
     def solve(self, cost: List[int]) -> int:
         n = len(cost) + 1
         cost.append(0)
@@ -21,5 +58,12 @@ class Solution:
 
 
 ob = Solution()
-print(ob.minCostClimbingStairs([10, 15, 20]))
-print(ob.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+# print(ob.minCostClimbingStairs([10, 15, 20]))
+# print(ob.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+# print(ob.recursive([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+# print(ob.recursive([10, 15, 20]))
+print(ob.recursive_with_memoization([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+print(ob.recursive_with_memoization([1, 100]))
+print(ob.recursive_with_memoization([10, 15, 20]))
+# print(ob.tab([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+# print(ob.tab([10,15,20]))
