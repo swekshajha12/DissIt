c7ce74981ca4e0c85dda762c5e2b7db5bb30e67f
swekshajha12 <jhasweksha12@gmail.com>
2023-11-16 17:28:52 +0530

    [ADD]

--- LeetCode75/binarySearch/base/firstAndLastOccurrence.py
+++ LeetCode75/binarySearch/base/firstAndLastOccurrence.py
@@ -5,7 +5,7 @@ from typing import List
 
 class Solution:
     def searchRangeFirst(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)-1
+        n = len(nums) - 1
         res = -1
         left, right = 0, n
         while left <= right:
@@ -23,7 +23,7 @@ class Solution:
         return res
 
     def searchRangeLast(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)-1
+        n = len(nums) - 1
         res = -1
         left, right = 0, n
         while left <= right:
@@ -51,8 +51,25 @@ class Solution:
             return [-1, -1]
         return [self.searchRangeFirst(nums, target), self.searchRangeLast(nums, target)]
 
+    def occurrenceCount(self, nums, target):
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1 and nums[0] == target:
+            return 1
+        elif len(nums) == 1 and nums[0] != target:
+            return 0
+        right_res = self.searchRangeLast(nums, target)
+        left_res = self.searchRangeFirst(nums, target)
+        if left_res >= 0 or right_res >= 0:
+            return right_res - left_res + 1
+
+        return 0
+
 
 ob = Solution()
 print(ob.searchRange([5, 7, 8, 8, 8, 10], 8))
 print(ob.searchRange([5, 7, 7, 8, 8, 10], 6))
-print(ob.searchRange([2,2], 6))
+print(ob.searchRange([2, 2], 6))
+print(ob.occurrenceCount([5, 7, 8, 8, 8, 10], 8))
+print(ob.occurrenceCount([5, 7, 7, 8, 8, 10], 6))
+print(ob.occurrenceCount([2, 2], 6))
